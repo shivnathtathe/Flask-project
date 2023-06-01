61da84ec05438aef3013d9a4a314c386fa82b095
Shiv Tathe <76526542+shivnathtathe@users.noreply.github.com>
2023-06-01 08:30:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+# URL_FOR used to create dynamic urls
+from flask import Flask, render_template, request
+
+flask_app = Flask(__name__, template_folder='templete')
+
+
+@flask_app.route('/', methods=['GET', 'POST'])
+def index():
+    # url = request.form.get('url')
+    # return render_template('index.html',url=url)
+    if request.method == 'POST':
+        url = request.form.get('url')
+        return render_template('index.html', url=url)
+
+    return render_template('index.html')
+
+
+if __name__ != "__main__":
+    pass
+else:
+    flask_app.run(port=2000, debug=True)
